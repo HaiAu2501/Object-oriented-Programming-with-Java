656b2b2c45298bd368338593f3cc7ace2895d552
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-04-12 22:27:51 +0700

    new

---
+++ Introduction/HelloWorldInterface.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorldInterface {
+    // Cho ra màn hình giao diện người dùng chứa chuỗi ở chính giữa màn hình
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+}
