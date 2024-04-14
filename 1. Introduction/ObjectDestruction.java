5edd4b926144dc624447a9c7cb415f2c2d9cda18
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-04-14 23:35:36 +0700

    new

---
+++ 1. Introduction/ObjectDestruction.java
@@ -0,0 +1,13 @@
+public class ObjectDestruction {
+    // Hủy đối tượng (Object Destruction) trong Java:
+    /*
+     * - Trong Java, việc hủy đối tượng được thực hiện tự động bởi garbage
+     * collector.
+     * - Garbage collector là một tiến trình chạy ngầm, quét các đối tượng không còn
+     * sử dụng và giải phóng bộ nhớ của chúng.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
