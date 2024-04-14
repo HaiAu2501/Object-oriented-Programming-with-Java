491dd5ba8dc0328f86c8802b7c29e77203d852d2
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-04-14 23:38:14 +0700

    new

---
+++ 2. Object-oriented Programming Principles/Overdding.java
@@ -0,0 +1,8 @@
+public class Overdding {
+    // Ghi đè phương thức (Method Overriding) trong Java:
+    /*
+     * - Ghi đè phương thức (Method Overriding) là cách tạo một phương thức trong
+     * lớp con có cùng tên, cùng tham số và cùng kiểu trả về với phương thức trong
+     * lớp cha.
+     */
+}
